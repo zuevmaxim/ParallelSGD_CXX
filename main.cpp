958effdcb1c7b10f6f3f179c807011e984d69a13
Maksim Zuev <maksim.zuev@jetbrains.com>
2022-01-08 17:56:14 +0300

    clean up

--- main.cpp
+++ main.cpp
@@ -7,8 +7,81 @@
 #include <chrono>
 #include <algorithm>
 #include <random>
+#include <libc.h>
+#include <memory>
 
 typedef double fp_type;
+
+/*! \brief Get the current nano-seconds since some epoch
+ * This is portable to OSX as well as Linux
+ * \return nsec since epoch
+ */
+inline unsigned long long CurrentNSec() {
+#ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec * 1000 *1000 *1000UL + tv.tv_usec*1000UL;
+#else
+    struct timespec ts;
+  clock_gettime(CLOCK_REALTIME, &ts);
+  return (unsigned long long) ts.tv_nsec +
+      (unsigned long long) ts.tv_sec * 1000 * 1000 * 1000;
+#endif
+}
+
+/*! \brief A simple timer which can be started and stopped
+ * Only read Clock.value after a stop or pause, otherwise use Read()
+ */
+class Clock {
+public:
+    double value; //< the elapsed time in seconds
+
+    Clock() : value(0), start(0), stop(0),  paused_(false) { }
+
+    /*! \brief Start counting.
+     */
+    inline void Start() {
+        start = CurrentNSec();
+        if (!paused_) {
+            value = 0.0;
+        }
+    }
+
+    /*! \brief Return the delta from the last call to Start()
+     */
+    inline double Read() {
+        if (paused_) {
+            return value;
+        } else {
+            stop = CurrentNSec();
+            value = (double) (stop - start) * 1e-9;
+            return value;
+        }
+    }
+
+    /*! \brief Pause counting, keep a running sum of time
+     */
+    inline double Pause() {
+        stop = CurrentNSec();
+        value += (double) (stop - start) * 1e-9;
+        paused_ = true;
+        return value;
+    }
+
+    /*! \brief Stop counting, next call to Start() will reset the time
+     */
+    inline double Stop() {
+        stop = CurrentNSec();
+        value = (double) (stop - start) * 1e-9;
+        paused_ = false;
+        return value;
+    }
+
+private:
+    unsigned long long start;
+    unsigned long long stop;
+    bool paused_;
+};
 
 struct Point {
     fp_type y{};
@@ -39,9 +112,9 @@ DataSet loadDataSet(const std::string& name) {
         indices.clear();
         values.clear();
         std::stringstream ss(str);
-        fp_type x;
-        int index;
-        char c;
+        fp_type x{};
+        int index{};
+        char c{};
         ss >> y;
         while (ss >> index >> c >> x) {
             indices.push_back(index);
@@ -68,7 +141,7 @@ DataSet loadDataSet(const std::string& name) {
     for (const auto& point : points) {
         dataSet.points.push_back(point);
     }
-    std::cout << "Dataset " << name << " loaded" << std::endl;
+    std::cout << "Dataset " << name << " loaded, size=" << dataSet.points.size() << std::endl;
     return dataSet;
 }
 
@@ -88,16 +161,35 @@ public:
                              iterations(iterations) {
     }
 
-    std::vector<fp_type> solve(const DataSet& train, const int* degrees) {
+    std::vector<fp_type> solve(DataSet& train, const int* degrees, Clock& clock) {
         this->degrees = degrees;
         int features = train.features;
         std::vector<fp_type> w(features, 0);
-
+        {
+            std::vector<std::atomic<int>> local_status(threads);
+            status.swap(local_status);
+        }
+        std::fill(status.begin(), status.end(), 2);
         std::vector<std::thread> tasks;
         tasks.reserve(threads);
         for (int i = 0; i < threads; ++i) {
-            tasks.emplace_back(&Solver::threadSolve, this, i, &w[0], features, &train);
+            tasks.emplace_back(&Solver::threadRoutine, this, i, &w[0], &train);
         }
+
+        auto rng = std::default_random_engine{};
+        for (int iteration = 0; iteration < iterations; ++iteration) {
+            std::shuffle(std::begin(train.points), std::end(train.points), rng);
+            clock.Start();
+            std::fill(status.begin(), status.end(), 1);
+
+            for (int i = 0; i < threads; ++i) {
+                while (status[i] != 2);
+            }
+            clock.Pause();
+            learningRate *= stepDecay;
+        }
+
+        std::fill(status.begin(), status.end(), 0);
 
         for (auto& task : tasks) {
             task.join();
@@ -117,26 +209,35 @@ public:
     }
 
 private:
-    void threadSolve(int threadId, fp_type* w, int wSize, const DataSet* train) const {
+    std::vector<std::atomic<int>> status;
+
+    void threadRoutine(int threadId, fp_type* w, const DataSet* train) {
+        while (true) {
+            if (status[threadId] == 0) return;
+            if (status[threadId] == 1) {
+                threadSolve(threadId, w, train);
+                status[threadId] = 2;
+            }
+        }
+    }
+
+    void threadSolve(int threadId, fp_type* w, const DataSet* train) const {
         const Point* points = &train->points[0];
         const int n = train->points.size();
         const int block = n / threads;
         const int start = threadId * block;
         const int end = threadId == threads - 1 ? n : std::min(n, start + block);
-        fp_type alpha = learningRate;
-        for (int iteration = iterations; iteration-- > 0;) {
-            for (int i = start; i < end; ++i) {
-                gradientStep(points[i], w, wSize, alpha);
-            }
-            alpha *= stepDecay;
+        const fp_type alpha = learningRate;
+        for (int i = start; i < end; ++i) {
+            gradientStep(points[i], w, alpha);
         }
     }
 
     inline static fp_type
-    dot(const fp_type* __restrict__ w,
+    dot(const fp_type* w,
         const int size,
-        const int* __restrict__ indices,
-        const fp_type* __restrict__ xs) {
+        const int* indices,
+        const fp_type* xs) {
         fp_type s = 0;
         for (int i = 0; i < size; ++i) {
             s += xs[i] * w[indices[i]];
@@ -145,10 +246,10 @@ private:
     }
 
     inline void
-    gradientStep(const Point& p, fp_type* __restrict__ w, const int wSize, const fp_type learningRate) const {
+    gradientStep(const Point& p, fp_type* w, const fp_type learningRate) const {
         const int size = p.indices.size();
-        const int* __restrict__ indices = &p.indices[0];
-        const fp_type* __restrict__ xs = &p.xs[0];
+        const int* indices = &p.indices[0];
+        const fp_type* xs = &p.xs[0];
 
         const fp_type wxy = dot(w, size, indices, xs) * p.y;
 
@@ -159,7 +260,7 @@ private:
             }
         }
 
-        int const* __restrict__ const degs = degrees;
+        int const* const degs = degrees;
 
         // update based on the evaluation
         const fp_type scalar = learningRate * 1.0;
@@ -171,7 +272,7 @@ private:
     }
 
 
-    const fp_type learningRate;
+    fp_type learningRate;
     const fp_type stepDecay;
     const int threads;
     const int iterations;
@@ -192,10 +293,9 @@ int main() {
     const int iterations = 100;
     for (int threads = 1; threads <= 16; threads *= 2) {
         Solver solver(0.5, 0.8, threads, iterations);
-        std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-        auto w = solver.solve(p.first, &degrees[0]);
-        std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-        int time_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() / double(iterations);
+        Clock clock;
+        auto w = solver.solve(p.first, &degrees[0], clock);
+        int time_ms = clock.Read() * 1e3 / iterations;
         if (threads == 1) {
             base_ms = time_ms;
         }
